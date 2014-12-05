1723cf443eb66d21b8e511e5bdd647810df35364
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2014-12-05 12:49:22 +0100

    windows conifg.h

---
+++ config.h
@@ -0,0 +1,8 @@
+// placeholder when compiling on windows
+
+#define HAVE_WINDOWS_H 1
+#define HAVE_GDIPLUS_H 1
+#define HAVE_SHELLAPI_H 1
+#define HAVE_WINBASE_H 1
+#define HAVE_OLE2_H 1
+#define HAVE_CODECVT 1
